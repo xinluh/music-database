afeb54328bc07a095133eb7c8deb4ad2ce67035d
xinlu.h <xinlu.h@46fa91e3-9f51-0410-9145-f5f9f43d5598>
2009-01-05 04:08:27 +0000

    Add some support for writing out libobj to mp3 tag. Also some comestic changes.

--- trunk/libdb/libobjs/Track.cs
+++ trunk/libdb/libobjs/Track.cs
@@ -14,6 +14,7 @@ namespace libdb
     [AutoUpdateClass(Tables.tblTrack)]
     public class Track : libobj
     {
+        public const string UnknownPath = "?";
         private Album album;
         private Piece piece;
         
@@ -113,38 +114,99 @@ namespace libdb
 
 
 
-        public void GetInfoFromFile(string filename)
+        public void ReadFromFile(string filename)
         {
             FileInfo finfo = new FileInfo(filename);
-            GetInfoFromFile(finfo);
+            ReadFromFile(finfo);
         }
 
-        public void GetInfoFromFile(FileInfo finfo)
+        public void ReadFromFile(FileInfo finfo)
         {
             if (finfo == null || !finfo.Exists ) 
             {
                 this.FileName = "?";
                 this.Size = 0;
                 this.Length = 0;
+				return;
             }
+			
+			// TODO: Check whether is valid mp3 file
+
+			Tag tag = new Tag(finfo.FullName);
 
             this.Size = (int) finfo.Length;
-
-            UltraID3 u = new UltraID3();
-            try
-            { 
-                u.Read(finfo.FullName);
-
-                this.Length = (int) System.Math.Round(u.Duration.TotalSeconds, 0);
-            }
-            catch (UltraID3Exception e)
-            {
-                Debug.HandleException(e); 
-            }
-
-
+			this.Length = (int) System.Math.Round(tag.UltraID3.Duration.TotalSeconds, 0);
+			this.FileName = finfo.FullName;
+			this.ID = tag.TrackID ?? 0;
+			this.TrackNumber = tag.TrackNum ?? 0;
+			this.Year = tag.Year ?? 0;
+			this.DiscNumber = tag.DiscNum ??  1;
         }
 
+        public void WriteToFile()
+        {
+            if (string.IsNullOrEmpty(FileName) || !File.Exists(FileName))
+                return;
+
+            if (Album == null)
+                throw new NullReferenceException("This track's parent album is null; cannot write to file!");
+
+            if (Piece == null)
+                throw new NullReferenceException("This track's piece infomation is null; cannot write to file!");
+
+            // TODO: Check whether is valid mp3 file
+
+            Tag tag = new Tag(FileName);
+            tag.Clear();
+
+            tag.Album = Album.Name;
+            tag.Title = Piece.Name;
+            tag.SetTrack(TrackNumber, Album.TotalTracks[DiscNumber - 1]);
+            tag.SetDiscNum(DiscNumber, Album.TotalDisc);
+
+            //is not a transcription
+            if (Piece.ParentPieceID == 0 || Piece.Composer.ID == Piece.ParentPieceID) //oops TODO!
+                tag.Artist = new string[] { Piece.Composer.GetName(Artist.NameFormats.Last_First) };
+            //is a transcritption
+            else
+            {
+                //todo!
+                tag.Artist = new string[] { Piece.ComposerNameWithTranscriptor, Piece.Composer.ArtistFullName, Piece.PrimaryPiece.Composer.ArtistFullName };
+            }
+
+            tag.Genre = Piece.Genre;
+            tag.Year = Year;
+            tag.AlbumArtist = "a"; //todo!
+            tag.Composers = Artist.GetNames(t.Performer, Artist.NameType.LastNameFirstName);
+
+            tag.ContentGroup = Piece.PieceName;
+            tag.Label = Label.GetLabelName(al.LabelID);
+            tag.UnsynchedLyric = Artist.GetName(t.Performer, Artist.NameType.FirstNameLastNameWithBriefArtistType, Constants.vbCrLf).Trim;
+            //tag.Comment = String.Join(vbCrLf, New String() {al.CDComment, t.TrackComment, Piece.ExtraInfo, Piece.Text})
+
+            tag.AlbumID = al.PrimaryKeyValue;
+            tag.TrackID = t.PrimaryKeyValue;
+            tag.AlbumType = al._AlbumType;
+            tag.IsComplete = al._Complete;
+            tag.IsInIpod = al._InIPOD;
+
+            if (IO.File.Exists(t.AssociatedAlbum.GetFilePath(Album.FilePaths.AlbumCoverPath_Auto)))
+            {
+                tag.AlbumPicture = new Bitmap(t.AssociatedAlbum.GetFilePath(Album.FilePaths.AlbumCoverPath_Auto));
+            }
+
+            tag.Write();
+
+            //update the file size after adding/changing tag
+            t.FileSize = new IO.FileInfo(path).Length;
+            t.SongLength = tag.UltraID3.Duration.TotalSeconds;
+            t.Update();
+
+            this.ID = tag.TrackID ?? 0;
+            this.TrackNumber = tag.TrackNum ?? 0;
+            this.Year = tag.Year ?? 0;
+            this.DiscNumber = tag.DiscNum ?? 1;
+        }
     }
 
 }
