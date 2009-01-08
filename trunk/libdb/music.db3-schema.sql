a05e5341fec1c077721b2c8fe70cf4cdf489d91c
xinlu.h <xinlu.h@46fa91e3-9f51-0410-9145-f5f9f43d5598>
2009-01-08 00:54:13 +0000

    add foreign key contraint triggers and tweak formatting in database

--- trunk/libdb/music.db3-schema.sql
+++ trunk/libdb/music.db3-schema.sql
@@ -6,18 +6,22 @@ CREATE TABLE 'tblAlbum' (
     "total_disc" INTEGER DEFAULT '1' NOT NULL,
     "total_track" TEXT NOT NULL,
     "complete" INTEGER DEFAULT '0' NOT NULL,
-    "label_id" INTEGER,
+    "label_id" INTEGER
+			   CONSTRAINT fk_label_id REFERENCES tblLabel(id),
     "in_ipod" INTEGER NOT NULL,
     "comment" TEXT,
-    "albumartist_id" INTEGER NOT NULL,
+    "albumartist_id" INTEGER NOT NULL
+			   CONSTRAINT fk_albumartist_id  REFERENCES tblArtist(id),
     "need_update" INTEGER NOT NULL DEFAULT '0',
     "created_date" TEXT NULL,
     "modified_date" TEXT
 );
 CREATE TABLE "tblArtist" (
     "id" INTEGER  PRIMARY KEY AUTOINCREMENT NOT NULL,
-    "name_id" INTEGER  NOT NULL,
-    "type_id" INTEGER  NOT NULL,
+    "name_id" INTEGER  NOT NULL
+			   CONSTRAINT fk_name_id  REFERENCES tblArtistName(id),
+    "type_id" INTEGER  NOT NULL
+			   CONSTRAINT fk_type_id  REFERENCES tblArtistType(id),
     "created_date" TEXT  NULL,
     "modified_date" TEXT  NULL,
     "temp" TEXT
@@ -49,12 +53,15 @@ CREATE TABLE "tblLabel" (
 );
 CREATE TABLE "tblPiece" (
     "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    "parent_piece_id" TEXT,
+    "parent_piece_id" TEXT
+			   CONSTRAINT fk_parent_piece_id  REFERENCES tblPiece(id),
     "name" TEXT NOT NULL,
     "old_name" TEXT,
     "connector" TEXT,
-    "composer_id" INTEGER NOT NULL,
-    "genre_id" INTEGER NOT NULL,
+    "composer_id" INTEGER NOT NULL
+			   CONSTRAINT fk_composer_id  REFERENCES tblArtist(id),
+    "genre_id" INTEGER NOT NULL
+			   CONSTRAINT fk_genre_id  REFERENCES tblGenre(id),
     "detail" TEXT,
     "extra" TEXT,
     "text" TEXT,
@@ -63,8 +70,10 @@ CREATE TABLE "tblPiece" (
     "temp" TEXT);
 CREATE TABLE "tblTrack" (
     "id" INTEGER  PRIMARY KEY AUTOINCREMENT NOT NULL,
-    "album_id" INTEGER  NOT NULL,
-    "piece_id" INTEGER  NOT NULL,
+    "album_id" INTEGER  NOT NULL
+			   CONSTRAINT fk_album_id  REFERENCES tblAlbum(id),
+    "piece_id" INTEGER  NOT NULL
+			   CONSTRAINT fk_piece_id  REFERENCES tblPiece(id),
     "disc_num" INTEGER  NOT NULL,
     "name" TEXT  NOT NULL,
     "track_num" TEXT  NOT NULL,
@@ -78,75 +87,208 @@ CREATE TABLE "tblTrack" (
     "created_date" TEXT  NULL,
     "modified_date" TEXT  NULL
 );
-CREATE VIEW "vwArtist" AS
-
-SELECT tblArtist.id AS id, 
-
-	tblArtistName.last_name AS last_name,
-
-	tblArtistName.first_name AS first_name,
-
-        tblArtistType.id AS type_id,
-
-	tblArtistType.name AS type,
-
-	tblArtistName.last_name || 
-
-	(CASE
-
-	WHEN (tblArtistName.first_name IS NOT NULL) THEN
-
-	 ', ' || tblArtistName.first_name
-
-	 ELSE
-
-	 ''
-
-	END) AS fullname,
-
-	tblArtistName.last_name || 
-
-	(CASE
-
-	WHEN (tblArtistName.first_name IS NOT NULL) THEN
-
-	 ', ' || tblArtistName.first_name
-
-	ELSE
-
-	''
-
-	END)
-
-	|| ' (' || tblArtistType.name || ')' AS fullname_type
-
-FROM tblArtist, tblArtistName, tblArtistType
-
-WHERE tblArtist.name_id = tblArtistName.id 
-
-and tblArtist.type_id = tblArtistType.id;
-CREATE VIEW "vwGenreComposer" AS SELECT DISTINCT 
-	tblPiece.genre_id AS id, 
-	tblPiece.composer_id AS composer_id, 
-	tblGenre.name AS name 
-FROM tblGenre, tblPiece 
+CREATE VIEW "vwGenreComposer" AS SELECT DISTINCT 
+	tblPiece.genre_id AS id, 
+	tblPiece.composer_id AS composer_id, 
+	tblGenre.name AS name 
+FROM tblGenre, tblPiece 
 WHERE tblPiece.genre_id = tblGenre.id;
+CREATE VIEW "vwartist" AS
+SELECT tblArtist.id AS id, 
+	tblArtistName.last_name AS last_name,
+	tblArtistName.first_name AS first_name,
+        tblArtistType.id AS type_id,
+	tblArtistType.name AS type,
+	tblArtistName.last_name || 
+	(CASE
+	WHEN (tblArtistName.first_name IS NOT NULL) THEN
+	 ', ' || tblArtistName.first_name
+	 ELSE
+	 ''
+	END) AS fullname,
+	tblArtistName.last_name || 
+	(CASE
+	WHEN (tblArtistName.first_name IS NOT NULL) THEN
+	 ', ' || tblArtistName.first_name
+	ELSE
+	''
+	END)
+	|| ' (' || tblArtistType.name || ')' AS fullname_type
+FROM tblArtist, tblArtistName, tblArtistType
+WHERE tblArtist.name_id = tblArtistName.id 
+and tblArtist.type_id = tblArtistType.id;
+CREATE TRIGGER fkd_tblAlbum_albumartist_id_tblArtist_id
+BEFORE DELETE ON tblArtist
+FOR EACH ROW BEGIN
+  SELECT RAISE(ROLLBACK, 'delete on table tblArtist violates foreign key constraint "fkd_tblAlbum_albumartist_id_tblArtist_id"')
+  WHERE (SELECT albumartist_id FROM tblAlbum WHERE albumartist_id = OLD.id) IS NOT NULL;
+END;
+CREATE TRIGGER fkd_tblAlbum_label_id_tblLabel_id
+BEFORE DELETE ON tblLabel
+FOR EACH ROW BEGIN
+  SELECT RAISE(ROLLBACK, 'delete on table tblLabel violates foreign key constraint "fkd_tblAlbum_label_id_tblLabel_id"')
+  WHERE (SELECT label_id FROM tblAlbum WHERE label_id = OLD.id) IS NOT NULL;
+END;
+CREATE TRIGGER fkd_tblArtist_name_id_tblArtistName_id
+BEFORE DELETE ON tblArtistName
+FOR EACH ROW BEGIN
+  SELECT RAISE(ROLLBACK, 'delete on table tblArtistName violates foreign key constraint "fkd_tblArtist_name_id_tblArtistName_id"')
+  WHERE (SELECT name_id FROM tblArtist WHERE name_id = OLD.id) IS NOT NULL;
+END;
+CREATE TRIGGER fkd_tblArtist_type_id_tblArtistType_id
+BEFORE DELETE ON tblArtistType
+FOR EACH ROW BEGIN
+  SELECT RAISE(ROLLBACK, 'delete on table tblArtistType violates foreign key constraint "fkd_tblArtist_type_id_tblArtistType_id"')
+  WHERE (SELECT type_id FROM tblArtist WHERE type_id = OLD.id) IS NOT NULL;
+END;
+CREATE TRIGGER fkd_tblPiece_composer_id_tblArtist_id
+BEFORE DELETE ON tblArtist
+FOR EACH ROW BEGIN
+  SELECT RAISE(ROLLBACK, 'delete on table tblArtist violates foreign key constraint "fkd_tblPiece_composer_id_tblArtist_id"')
+  WHERE (SELECT composer_id FROM tblPiece WHERE composer_id = OLD.id) IS NOT NULL;
+END;
+CREATE TRIGGER fkd_tblPiece_genre_id_tblGenre_id
+BEFORE DELETE ON tblGenre
+FOR EACH ROW BEGIN
+  SELECT RAISE(ROLLBACK, 'delete on table tblGenre violates foreign key constraint "fkd_tblPiece_genre_id_tblGenre_id"')
+  WHERE (SELECT genre_id FROM tblPiece WHERE genre_id = OLD.id) IS NOT NULL;
+END;
+CREATE TRIGGER fkd_tblPiece_parent_piece_id_tblPiece_id
+BEFORE DELETE ON tblPiece
+FOR EACH ROW BEGIN
+  SELECT RAISE(ROLLBACK, 'delete on table tblPiece violates foreign key constraint "fkd_tblPiece_parent_piece_id_tblPiece_id"')
+  WHERE (SELECT parent_piece_id FROM tblPiece WHERE parent_piece_id = OLD.id) IS NOT NULL;
+END;
+CREATE TRIGGER fkd_tblTrack_album_id_tblAlbum_id
+BEFORE DELETE ON tblAlbum
+FOR EACH ROW BEGIN
+  SELECT RAISE(ROLLBACK, 'delete on table tblAlbum violates foreign key constraint "fkd_tblTrack_album_id_tblAlbum_id"')
+  WHERE (SELECT album_id FROM tblTrack WHERE album_id = OLD.id) IS NOT NULL;
+END;
+CREATE TRIGGER fkd_tblTrack_piece_id_tblPiece_id
+BEFORE DELETE ON tblPiece
+FOR EACH ROW BEGIN
+  SELECT RAISE(ROLLBACK, 'delete on table tblPiece violates foreign key constraint "fkd_tblTrack_piece_id_tblPiece_id"')
+  WHERE (SELECT piece_id FROM tblTrack WHERE piece_id = OLD.id) IS NOT NULL;
+END;
+CREATE TRIGGER fki_tblAlbum_albumartist_id_tblArtist_id
+BEFORE INSERT ON [tblAlbum]
+FOR EACH ROW BEGIN
+  SELECT RAISE(ROLLBACK, 'insert on table tblAlbum violates foreign key constraint "fki_tblAlbum_albumartist_id_tblArtist_id"')
+  WHERE (SELECT id FROM tblArtist WHERE id = NEW.albumartist_id) IS NULL;
+END;
+CREATE TRIGGER fki_tblAlbum_label_id_tblLabel_id
+BEFORE INSERT ON [tblAlbum]
+FOR EACH ROW BEGIN
+  SELECT RAISE(ROLLBACK, 'insert on table tblAlbum violates foreign key constraint "fki_tblAlbum_label_id_tblLabel_id"')
+  WHERE NEW.label_id IS NOT NULL AND (SELECT id FROM tblLabel WHERE id = NEW.label_id) IS NULL;
+END;
+CREATE TRIGGER fki_tblArtist_name_id_tblArtistName_id
+BEFORE INSERT ON [tblArtist]
+FOR EACH ROW BEGIN
+  SELECT RAISE(ROLLBACK, 'insert on table "tblArtist" violates foreign key constraint "fki_tblArtist_name_id_tblArtistName_id"')
+  WHERE (SELECT id FROM tblArtistName WHERE id = NEW.name_id) IS NULL;
+END;
+CREATE TRIGGER fki_tblArtist_type_id_tblArtistType_id
+BEFORE INSERT ON [tblArtist]
+FOR EACH ROW BEGIN
+  SELECT RAISE(ROLLBACK, 'insert on table tblArtist violates foreign key constraint "fki_tblArtist_type_id_tblArtistType_id"')
+  WHERE (SELECT id FROM tblArtistType WHERE id = NEW.type_id) IS NULL;
+END;
+CREATE TRIGGER fki_tblPiece_composer_id_tblArtist_id
+BEFORE INSERT ON [tblPiece]
+FOR EACH ROW BEGIN
+  SELECT RAISE(ROLLBACK, 'insert on table tblPiece violates foreign key constraint "fki_tblPiece_composer_id_tblArtist_id"')
+  WHERE (SELECT id FROM tblArtist WHERE id = NEW.composer_id) IS NULL;
+END;
+CREATE TRIGGER fki_tblPiece_genre_id_tblGenre_id
+BEFORE INSERT ON [tblPiece]
+FOR EACH ROW BEGIN
+  SELECT RAISE(ROLLBACK, 'insert on table tblPiece violates foreign key constraint "fki_tblPiece_genre_id_tblGenre_id"')
+  WHERE (SELECT id FROM tblGenre WHERE id = NEW.genre_id) IS NULL;
+END;
+CREATE TRIGGER fki_tblPiece_parent_piece_id_tblPiece_id
+BEFORE INSERT ON [tblPiece]
+FOR EACH ROW BEGIN
+  SELECT RAISE(ROLLBACK, 'insert on table tblPiece violates foreign key constraint "fki_tblPiece_parent_piece_id_tblPiece_id"')
+  WHERE NEW.parent_piece_id IS NOT NULL AND (SELECT id FROM tblPiece WHERE id = NEW.parent_piece_id) IS NULL;
+END;
+CREATE TRIGGER fki_tblTrack_album_id_tblAlbum_id
+BEFORE INSERT ON [tblTrack]
+FOR EACH ROW BEGIN
+  SELECT RAISE(ROLLBACK, 'insert on table tblTrack violates foreign key constraint "fki_tblTrack_album_id_tblAlbum_id"')
+  WHERE (SELECT id FROM tblAlbum WHERE id = NEW.album_id) IS NULL;
+END;
+CREATE TRIGGER fki_tblTrack_piece_id_tblPiece_id
+BEFORE INSERT ON [tblTrack]
+FOR EACH ROW BEGIN
+  SELECT RAISE(ROLLBACK, 'insert on table tblTrack violates foreign key constraint "fki_tblTrack_piece_id_tblPiece_id"')
+  WHERE (SELECT id FROM tblPiece WHERE id = NEW.piece_id) IS NULL;
+END;
+CREATE TRIGGER fku_tblAlbum_albumartist_id_tblArtist_id
+BEFORE UPDATE ON [tblAlbum]
+FOR EACH ROW BEGIN
+    SELECT RAISE(ROLLBACK, 'update on table tblAlbum violates foreign key constraint "fku_tblAlbum_albumartist_id_tblArtist_id"')
+      WHERE (SELECT id FROM tblArtist WHERE id = NEW.albumartist_id) IS NULL;
+END;
+CREATE TRIGGER fku_tblAlbum_label_id_tblLabel_id
+BEFORE UPDATE ON [tblAlbum]
+FOR EACH ROW BEGIN
+    SELECT RAISE(ROLLBACK, 'update on table tblAlbum violates foreign key constraint "fku_tblAlbum_label_id_tblLabel_id"')
+      WHERE NEW.label_id IS NOT NULL AND (SELECT id FROM tblLabel WHERE id = NEW.label_id) IS NULL;
+END;
+CREATE TRIGGER fku_tblArtist_name_id_tblArtistName_id
+BEFORE UPDATE ON [tblArtist]
+FOR EACH ROW BEGIN
+    SELECT RAISE(ROLLBACK, 'update on table tblArtist violates foreign key constraint "fku_tblArtist_name_id_tblArtistName_id"')
+      WHERE (SELECT id FROM tblArtistName WHERE id = NEW.name_id) IS NULL;
+END;
+CREATE TRIGGER fku_tblArtist_type_id_tblArtistType_id
+BEFORE UPDATE ON [tblArtist]
+FOR EACH ROW BEGIN
+    SELECT RAISE(ROLLBACK, 'update on table tblArtist violates foreign key constraint "fku_tblArtist_type_id_tblArtistType_id"')
+      WHERE (SELECT id FROM tblArtistType WHERE id = NEW.type_id) IS NULL;
+END;
+CREATE TRIGGER fku_tblPiece_composer_id_tblArtist_id
+BEFORE UPDATE ON [tblPiece]
+FOR EACH ROW BEGIN
+    SELECT RAISE(ROLLBACK, 'update on table tblPiece violates foreign key constraint "fku_tblPiece_composer_id_tblArtist_id"')
+      WHERE (SELECT id FROM tblArtist WHERE id = NEW.composer_id) IS NULL;
+END;
+CREATE TRIGGER fku_tblPiece_genre_id_tblGenre_id
+BEFORE UPDATE ON [tblPiece]
+FOR EACH ROW BEGIN
+    SELECT RAISE(ROLLBACK, 'update on table tblPiece violates foreign key constraint "fku_tblPiece_genre_id_tblGenre_id"')
+      WHERE (SELECT id FROM tblGenre WHERE id = NEW.genre_id) IS NULL;
+END;
+CREATE TRIGGER fku_tblPiece_parent_piece_id_tblPiece_id
+BEFORE UPDATE ON [tblPiece]
+FOR EACH ROW BEGIN
+    SELECT RAISE(ROLLBACK, 'update on table tblPiece violates foreign key constraint "fku_tblPiece_parent_piece_id_tblPiece_id"')
+      WHERE NEW.parent_piece_id IS NOT NULL AND (SELECT id FROM tblPiece WHERE id = NEW.parent_piece_id) IS NULL;
+END;
+CREATE TRIGGER fku_tblTrack_album_id_tblAlbum_id
+BEFORE UPDATE ON [tblTrack]
+FOR EACH ROW BEGIN
+    SELECT RAISE(ROLLBACK, 'update on table tblTrack violates foreign key constraint "fku_tblTrack_album_id_tblAlbum_id"')
+      WHERE (SELECT id FROM tblAlbum WHERE id = NEW.album_id) IS NULL;
+END;
+CREATE TRIGGER fku_tblTrack_piece_id_tblPiece_id
+BEFORE UPDATE ON [tblTrack]
+FOR EACH ROW BEGIN
+    SELECT RAISE(ROLLBACK, 'update on table tblTrack violates foreign key constraint "fku_tblTrack_piece_id_tblPiece_id"')
+      WHERE (SELECT id FROM tblPiece WHERE id = NEW.piece_id) IS NULL;
+END;
 CREATE TRIGGER "tr_archive_old_name"
    AFTER UPDATE OF name
    ON main.tblPiece
 BEGIN
-
     UPDATE tblPiece SET old_name =  
-
     CASE
-
     WHEN (old_name IS NULL) THEN
-    old.name || '|'
-
+         old.name || '|'
     ELSE
-
-    old_name || old.name || '|' 
-
+         old_name || old.name || '|' 
     END  WHERE id = old.id;
 END;
 CREATE TRIGGER "tr_created_date_album"
@@ -170,9 +312,7 @@ CREATE TRIGGER "tr_created_date_artistname"
    ON main.tblArtistName
 BEGIN
     UPDATE tblArtistName
-
     SET created_date = strftime('%m/%d/%Y %H:%M','now', 'localtime')
-
     WHERE id = new.id;
 END;
 CREATE TRIGGER "tr_created_date_artisttype"
@@ -180,9 +320,7 @@ CREATE TRIGGER "tr_created_date_artisttype"
    ON main.tblArtistType
 BEGIN
     UPDATE tblArtistType
-
     SET created_date = strftime('%m/%d/%Y %H:%M','now', 'localtime')
-
     WHERE id = new.id;
 END;
 CREATE TRIGGER "tr_created_date_label"
@@ -190,9 +328,7 @@ CREATE TRIGGER "tr_created_date_label"
    ON main.'tblLabel'
 BEGIN
     UPDATE tblLabel
-
     SET created_date = strftime('%m/%d/%Y %H:%M','now', 'localtime')
-
     WHERE id = new.id;
 END;
 CREATE TRIGGER "tr_created_date_piece"
@@ -200,9 +336,7 @@ CREATE TRIGGER "tr_created_date_piece"
    ON main.'tblPiece'
 BEGIN
     UPDATE tblPiece
-
     SET created_date = strftime('%m/%d/%Y %H:%M','now', 'localtime')
-
     WHERE id = new.id;
 END;
 CREATE TRIGGER "tr_created_date_track"
@@ -286,59 +420,48 @@ CREATE TRIGGER "tr_set_mod_date_album"
 BEGIN
     UPDATE tblAlbum 
     SET modified_date = 
-
-    strftime('%m/%d/%Y %H:%M','now', 'localtime')
+        strftime('%m/%d/%Y %H:%M','now', 'localtime')
     WHERE id = new.id;
 END;
 CREATE TRIGGER "tr_set_mod_date_artist"
    AFTER  UPDATE ON main.tblArtist
 BEGIN
     UPDATE tblArtist
-    SET modified_date =
-    strftime('%m/%d/%Y %H:%M','now', 'localtime')
+    SET modified_date =  strftime('%m/%d/%Y %H:%M','now', 'localtime')
     WHERE id = new.id;
 END;
 CREATE TRIGGER "tr_set_mod_date_artistname"
    AFTER  UPDATE ON main.tblArtistName
 BEGIN
     UPDATE tblArtistName
-    SET modified_date = 
-
-    strftime('%m/%d/%Y %H:%M','now', 'localtime')
+    SET modified_date = strftime('%m/%d/%Y %H:%M','now', 'localtime')
     WHERE id = new.id;
 END;
 CREATE TRIGGER "tr_set_mod_date_artisttype"
    AFTER  UPDATE ON main.tblArtistType
 BEGIN
     UPDATE tblArtistType
-    SET modified_date = 
-
-    strftime('%m/%d/%Y %H:%M','now', 'localtime')
+    SET modified_date = strftime('%m/%d/%Y %H:%M','now', 'localtime')
     WHERE id = new.id;
 END;
 CREATE TRIGGER "tr_set_mod_date_label"
    AFTER  UPDATE ON main.tblLabel
 BEGIN
     UPDATE tblLabel
-    SET modified_date = 
-
-    strftime('%m/%d/%Y %H:%M','now', 'localtime')
+    SET modified_date = strftime('%m/%d/%Y %H:%M','now', 'localtime')
     WHERE id = new.id;
 END;
 CREATE TRIGGER "tr_set_mod_date_piece"
    AFTER  UPDATE ON main.tblpiece
 BEGIN
     UPDATE tblpiece
-    SET modified_date = 
-
-    strftime('%m/%d/%Y %H:%M','now', 'localtime')
+    SET modified_date = strftime('%m/%d/%Y %H:%M','now', 'localtime')
     WHERE id = new.id;
 END;
 CREATE TRIGGER "tr_set_mod_date_track"
    AFTER  UPDATE ON main.tblTrack
 BEGIN
     UPDATE tblTrack
-    SET modified_date =
-    strftime('%m/%d/%Y %H:%M','now', 'localtime')
+    SET modified_date = strftime('%m/%d/%Y %H:%M','now', 'localtime')
     WHERE id = new.id;
 END;
